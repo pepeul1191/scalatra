0db0c51f86c3a9a53ccd298a996752f57ed70970
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-24 12:25:00 -0500

    Segundo servlet

--- src/main/scala/pe/softweb/servlets/SistemaServlet.scala
+++ src/main/scala/pe/softweb/servlets/SistemaServlet.scala
@@ -2,7 +2,7 @@ package pe.softweb.servlets
 
 import org.scalatra._
 
-class AppServlet extends ScalatraServlet {
+class SistemaServlet extends ScalatraServlet {
 
   get("/") {
     views.html.hello()
@@ -11,6 +11,6 @@ class AppServlet extends ScalatraServlet {
   get("/hello/:name") {
 	  // Matches "GET /hello/foo" and "GET /hello/bar"
 	  // params("name") is "foo" or "bar"
-	  <p>Hello, {params("name")}</p>
+	  <p>Hello Sistema, {params("name")}</p>
 	}
 }
