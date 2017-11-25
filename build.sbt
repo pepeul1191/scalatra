91df4450b9938593bc5c2478d403a2ae2e46a922
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-25 13:04:24 -0500

    Intentando usar SlickORM

--- build.sbt
+++ build.sbt
@@ -15,9 +15,14 @@ libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
   "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
   "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
-  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
-  "mysql" % "mysql-connector-java" % "5.1.10"
+  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
 )
+
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.10"
+libraryDependencies += "com.typesafe.slick" % "slick_2.12" % "3.2.1"
+libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.6.4"
 
 enablePlugins(SbtTwirl)
 enablePlugins(ScalatraPlugin)
+
+containerPort in Jetty := 8090
--- src/main/scala/pe/softweb/servlets/SistemaServlet.scala
+++ src/main/scala/pe/softweb/servlets/SistemaServlet.scala
@@ -2,6 +2,17 @@ package pe.softweb.servlets
 
 import org.scalatra._
 import pe.softweb.utils._
+import slick.driver.SQLiteDriver.api._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class Sistema(tag: Tag) extends Table[(Int, String, String, String)](tag, "sistemas") {
+  def id = column[Int]("id", O.PrimaryKey) // This is the primary key column
+  def nombre = column[String]("nombre")
+  def version = column[String]("version")
+  def repositorio = column[String]("repositorio")
+  // Every table needs a * projection with the same type as the table's type parameter
+  def * = (id, nombre, version, repositorio)
+}
 
 class SistemaServlet extends ScalatraServlet {
 
@@ -16,7 +27,25 @@ class SistemaServlet extends ScalatraServlet {
 	}
 
 	get("/listar"){
-		ScalaJdbcConnectSelect.select
+		//ScalaJdbcConnectSelect.select
 		<p>lista</p>	
+	}
+
+	get("/demo"){
+		
+		val db = Database.forURL("jdbc:sqlite:db/db_accesos.db", driver = "org.sqlite.JDBC")
+		val sistemas = TableQuery[Sistema]
+		try {
+		  // ...
+		  println("antes!!!")
+		  db.run(sistemas.result).map(_.foreach {
+			  case (id, nombre, version, repositorio) =>
+			  	println("!+")
+			    println("  " + id + "\t" + nombre + "\t" + version + "\t" + repositorio)
+			})
+			println("despues!!!")
+		} finally db.close
+		
+		<p>demo</p>	
 	}
 }
